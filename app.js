c21af4f39da81e8e6f052235155422f15187d65f
O08 <47027217+O08@users.noreply.github.com>
2022-12-13 13:39:36 +0800

    code_20221213: index.html--dan

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,13 +14,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// app.use('/', "'./public/home.html'");
+// app.use('/users', usersRouter);
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+"/dist"+"/home.html")        //设置/ 下访问文件位置
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
