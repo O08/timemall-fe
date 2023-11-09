f8cb83673dbab61ddf601a4ca4ee67fd979f9e29
O08 <47027217+O08@users.noreply.github.com>
2023-11-09 14:59:05 +0800

    code_20231109: baidu tracker --dan

--- public/common/javascripts/tm.js
+++ public/common/javascripts/tm.js
@@ -26,6 +26,17 @@ import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
     })
   })()
 
+// baidu track
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?606e2a200184c22d0684dda57e5323f8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+
 
 
 
