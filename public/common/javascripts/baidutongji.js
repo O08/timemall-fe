35e6cd3f63afeaa282c9a7ccd80bbcb11343262d
O08 <47027217+O08@users.noreply.github.com>
2024-05-17 10:51:26 +0800

    code_20240517: seo optimize --dan

---
+++ public/common/javascripts/baidutongji.js
@@ -0,0 +1,8 @@
+// baidu track
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?606e2a200184c22d0684dda57e5323f8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
